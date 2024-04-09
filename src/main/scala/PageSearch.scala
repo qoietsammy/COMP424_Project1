cacad01334199ae511a3a825ff60d3e589f1d2f5
qoietsammy <beerssa20@gcc.edu>
2024-04-09 18:50:26 -0400

    simple count implemented

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -4,11 +4,23 @@ import scala.collection.parallel.CollectionConverters._
 object PageSearch {
     /**
      * @param pages  a list of RankedWebPage objects to be searched
-     * @param query  a list of search terms to be counted in those pages
+     * @param queries  a list of search terms to be counted in those pages
      * @return       a list of the number of times any of the terms appeared in each page in the same order as given
      */
-    def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        List() // TODO: implement this method and remove this stub
+    def count(pages: List[RankedWebPage], queries: List[String]): List[Double] = {
+
+        // counts the number of times a single query term occurs in the page
+        def countQuery(pageText: String, query: String): Double = {
+            pageText.split(" ").count(_.toLowerCase() == query.toLowerCase())
+        }
+
+        // counts the number of query terms in the entire page
+        def countPage(page: RankedWebPage): Double = {
+            queries.map(query => countQuery(page.text.toLowerCase(), query)).sum
+        }
+
+        // creates a list of each total count
+        pages.map(countPage(_))
     }
 
     /**
