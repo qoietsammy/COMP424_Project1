0eb0678e91a615d67dbd7c56da228d3e0fb482a9
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2024-04-19 13:17:47 -0400

    indegree working

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -7,10 +7,6 @@ object PageRank {
      * @return      A map of page.id to a weight of 1.0 for those same WebPage objects
      */
     def equal(pages: Map[String, WebPage]): Map[String, Double] = {
-//        val naiveMap = pages.foldLeft(Map.empty[String,Double]){
-//            case (map, (_1, value: WebPage)) =>
-//                map + (_1 -> 1.0)}
-//        naiveMap
         pages.map({case(_1,_) => _1 -> 1.0})
 
     }
@@ -20,7 +16,11 @@ object PageRank {
      * @return A map of page.id to a weight that is a simple count of the number of pages linking to that page
      */
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
-        Map() // TODO: remove this stub and implement this method
+        pages.map({case(id: String, page:WebPage) =>
+          id -> pages.values.foldLeft(0)((counter : Int, web : WebPage) =>
+            if web.links.contains(id) then counter + 1 else counter
+          ).toDouble
+        })
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
